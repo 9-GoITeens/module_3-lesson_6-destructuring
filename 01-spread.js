056c2c6dd3e29fc49f8455f1d8d6411609505757
Oksana Petryshyn <opetryshyn2@gmail.com>
2023-04-30 09:56:00 +0300

    робота на уроці

--- 01-spread.js
+++ 01-spread.js
@@ -2,27 +2,47 @@
  * Операція spread (розпилення)
  * - Array.prototype.concat() і аналог через spread
  */
+// const numbers = [1, 2, 3].concat([7, 8, 9], [4, 5, 6]);
+
+// const numbers = [...[1, 2, 3], ...[1, 2, 3], 4, 5, 6, ...[1, 2, 3]];
+
+// console.log(numbers);
 
 /*
  * Пошук най меншої або най більшої температури (числа)
  */
 const temps = [18, 14, 12, 21, 17, 29, 24];
 
+// console.log(Math.min(...temps));
+// console.log(Math.max(...temps));
+
+// console.log(Math.max(1, 5, 17, 3));
+// console.log(Math.min(1, 5, 17, 3));
+
 /*
- * Зшиваємо декілька масивів массивов в один через spread
+ * Зшиваємо декілька масивів в один через spread
  */
-const lastWeekTemps = [1, 2, 3];
+const lastWeekTemps = [{ a: 1, b: 2, c: 3 }];
 const currentTemps = [4, 5, 6];
 const nextWeekTemps = [7, 8, 9];
 
-const allTemps = [];
+// const allTemps = [lastWeekTemps, currentTemps, nextWeekTemps];
+const allTemps = [...currentTemps, ...lastWeekTemps, ...nextWeekTemps];
 // console.log(allTemps);
 
 /*
  * Розпилення обєктів spread
  */
-const a = { x: 1, y: 2 };
 const b = { x: 0, z: 3 };
+const a = { x: 1, y: 2 };
+
+const c = {
+  ...b,
+  mame: 'Mango',
+  ...a,
+};
+
+// console.log(c);
 
 //наприклад
 // є системні налаштування
@@ -37,6 +57,10 @@ const userSettings = {
   hideSidebar: true,
 };
 // в результаті базові залишились і ті що змінились
-const finalSettings = {};
+const finalSettings = {
+  theme: 'light',
+  showNotifications: false,
+  hideSidebar: true,
+};
 
 // console.log(finalSettings);
--- 02-destructuring.js
+++ 02-destructuring.js
@@ -4,13 +4,26 @@
  * - Імя змінної відмінне від імені властивості
  */
 
-const playlist = {
-  name: 'Мій супер плейлист',
-  rating: 5,
-  tracks: ['трек-1', 'трек-2', 'трек-3'],
-  trackCount: 3,
-  autor: 'Mango',
-};
+// const playlist = {
+//   name: 'Мій супер плейлист',
+//   rating: 5,
+//   tracks: ['трек-1', 'трек-2', 'трек-3'],
+//   trackCount: 3,
+//   // autor: 'Polly',
+// };
+
+// const {
+//   rating,
+//   tracks,
+//   name,
+//   trackCount: trackOfCount = 0,
+//   autor = 'Mango',
+// } = playlist;
+
+// console.log(rating, tracks, name, trackOfCount, autor);
+
+// const {} = obj;
+// const [] = ar;
 
 /*
  * Глибока деструктуризація
@@ -28,18 +41,42 @@ const playlist = {
 //   },
 // };
 
+// const {
+//   name,
+//   tag,
+//   location,
+//   avatar,
+//   stats: { followers, views, likes },
+// } = profile;
+
+// console.log(name, tag, location, avatar, followers, views, likes);
+
 /*
  * Деструктуризація масивів
  */
 
 const rgb = [255, 100, 80];
 
-// const authors = {
-//   kiwi: 4,
-//   poly: 7,
-//   ajax: 9,
-//   mango: 6,
-// };
+const [a, b, c] = rgb;
+
+// console.log(a, b, c);
+
+const authors = {
+  kiwi: 4,
+  poly: 7,
+  ajax: 9,
+  mango: 6,
+};
+
+const entries = Object.entries(authors);
+for (const entry of entries) {
+  const name = entry[0];
+  const rating = entry[1];
+
+  // console.log(name, rating);
+}
+
+// console.log(entries);
 
 /*
  * Операція rest (збір)
@@ -56,6 +93,15 @@ const rgb = [255, 100, 80];
 //   },
 // };
 
+// const { name, tag, location, ...restProps } = profile;
+
+// console.log(name, tag, location);
+
+// const restProps = {
+//   auror: profile.avatar,
+//   stats: profile.stats,
+// };
+
 // console.log(restProps);
 // console.log(profile);
 
@@ -66,7 +112,22 @@ const rgb = [255, 100, 80];
  */
 
 const showProfileInfo = function (userProfile) {
-  //   console.log(userProfile);
+  const { name, tag, location, ...resProps } = userProfile;
+
+  console.log(name, tag, location);
+  console.log(resProps);
 };
 
-// showProfileInfo(profile);
+const profile = {
+  name: 'Jacques Gluke',
+  tag: 'jgluke',
+  location: 'Ocho Rios, Jamaica',
+  avatar: 'https://s3.amazonaws.com/uifaces/faces/twitter/r_oy/128.jpg',
+  stats: {
+    followers: 5603,
+    views: 4827,
+    likes: 1308,
+  },
+};
+
+showProfileInfo(profile);
